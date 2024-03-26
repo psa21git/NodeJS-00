66d9140fc110c2634b98a5ee699be3a95db32599
psa_code <p.sreyanshuspazz@gmail.com>
2024-03-26 11:53:54 +0530

    POSTMAN

--- project-01/index.js
+++ project-01/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const fs = require('fs')
 const users = require("./MOCK_DATA.json")
 const app = express();
 const PORT = 8000;
+
+//MiddleWare
+app.use(express.urlencoded( { extended: false } )); //Parse URL
 
 //Routes
 app
@@ -12,18 +16,39 @@ app
         if (!user) return res.status(404).send({ message: 'User not found' });
         else res.json(user)
     })
-    .put((req, res) => {
-        // TOOD: create user
-        return res.json({status: "Pending"})
-    })
     .patch((req, res) => {
         // TOOD: Update user
-        return res.json({status: "Pending"})
+        const updateId = Number(req.params.id)
+        users.forEach((user)=>{
+            if(user.id === updateId){
+                user.first_name = req.query.first_name
+                user.last_name = req.query.last_name
+                user.email = req.query.email
+            }
+        })
+        fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err,data) => {
+            return res.json({status: "user updated",id:updateId})
+        })
     })
     .delete((req, res) => {
         // TOOD: delete user
-        return res.json({status: "Pending"})
+        const deleteId = Number(req.params.id)
+        users.forEach((user)=>{
+            if(user.id === deleteId) users.pop(user)
+        })
+        fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err,data) => {
+            return res.json({status: "user deleted",id:deleteId})
+        })
     })
+
+app.post( '/api/users', ( req, res )=>{
+    // TOOD: Create user
+    const body = req.body
+    users.push({...body,id: users.length+1})
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err,data) => {
+        return res.json({status: "user added",id:users.length})
+    })
+})
 
 
 app.get('/users', (req, res) => {
